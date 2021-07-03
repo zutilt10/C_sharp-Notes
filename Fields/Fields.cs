4f5a0d45e5a9b924e213acda1bb54b45f488e34a
zutilt10 <123jonathanwoo34@gmail.com>
2021-07-03 18:21:42 +0800

    Added both Fields and Properties

---
+++ Fields/Fields.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Fields
+{
+    public class Customer
+    {
+        // This shows a method for overloading constructors
+        // All have different parameters for different attributes
+        // We can only call one of them for one instance of the class
+        // We can also initialise lists/arrays or set certain values of certain variables no matter what constructor is used
+        public int Id;
+        public string Name;
+        // With the readonly modifier, we can set a certain field to be initiated only once
+        // This will help to improve the robustness of our code
+        public readonly List<Order> Orders = new List<Order>();
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        // By adding the this keyword underneath the constructor's statement, we set the id like we did in the above constructor 
+        public Customer(int id, string name)
+            :this(id)
+        {
+            this.Name = name;
+        }
+    }
+
+    public class Order
+    {
+
+    }
+
+    class Fields
+    {
+
+    }
+}
---
+++ Properties/Properties.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Properties
+{
+    public class Person
+    {
+        // Within classes, there are also things called properties
+        // They function like attributes but may not be accessible by an instance of the class
+        // Instead, we may have to use a method to do so
+        // One advantage that we have with properties over simple attributes is that we can add logic to how the properties are derived
+
+        // Below is a quick way to set a property
+        // The Name property can be accessed by instances of the class easily
+        // This means that we can change the property in the Main method with an instance of the class(Setting the property)
+        // We can also access it's current value with an instance(Getting the property) 
+        public string Name { get; set; }
+        // If we want to we can set the property to be "un-setable" by any instance of the class
+        // Simply add private to the set
+        public DateTime Birthdate { get; private set; }
+
+        // When we make a property private like that, we have to find another way to set it
+        // Like with a constructor
+        public Person(DateTime birthDate)
+        {
+            Birthdate = birthDate;
+        }
+
+
+        // This is an example of a property with logic integrated
+        // It will automatically calculate it's own value without needing input from the user or programmer
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+
+    class Properties
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(2012, 1, 1));
+            person.Name = "John";
+
+        }
+    }
+}
