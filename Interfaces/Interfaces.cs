6b53a6dd6bbf7cfec84622181b6c7bd9d775d3a0
zutilt10 <123jonathanwoo34@gmail.com>
2021-07-13 11:12:56 +0800

    Added Interfaces

---
+++ Interfaces/Interfaces.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Interfaces
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime DatePlaced { get; set; }
+        public Shipment Shipment { get; set; }
+        public float TotalPrice { get; set; }
+
+        public bool IsShipped
+        {
+            get { return Shipment != null; }
+        }
+    }
+    public interface IShippingCalculator
+    {
+        float CalculateShipping(Order order);
+    }
+
+    public class ShippingCalculator : IShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            if (order.TotalPrice < 35f)
+            {
+                return order.TotalPrice * 0.1f;
+            }
+            return 0;
+        }
+    }
+
+    public class OrderProcessor
+    {
+        private readonly IShippingCalculator _shippingCalculator;
+
+        public OrderProcessor(IShippingCalculator shippingCalculator)
+        {
+            _shippingCalculator = shippingCalculator;
+        }
+
+        public void Process(Order order)
+        {
+            if (order.IsShipped)
+            {
+                throw new InvalidOperationException("This order has already been processed");
+            }
+            order.Shipment = new Shipment;
+            {
+                Cost = _shippingCalculator.CalculateShipping(order);
+                ShippingDate = DateTime.Today.AddDays(1);
+            }
+        }
+    }
+    class Interfaces
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
